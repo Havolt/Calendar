1a949c56ace8ccd7d213782e9da39988cb0ab0a6
Mark Fitzpatrick <markfitz815@gmail.com>
2017-11-23 15:10:40 +0000

    getMonth now functional and outputs any month and year

--- calScript.js
+++ calScript.js
@@ -5,6 +5,10 @@ const colVal = 6;
 const rowVal = 7;
 const app = document.getElementById('app');
 
+let monthRun = 0;
+let cD = new Date();
+
+
 
 
 
@@ -17,9 +21,22 @@ function createGeneral(){
   calHeader.appendChild(monthDis);
 }
 
-function getMonth(){
+function getMonth(mon, year){
 
-}
+  if(mon == 0){monthDisp.innerHTML = 'January';}
+  else if(mon == 1){monthDisp.innerHTML = 'February';}
+  else if(mon == 2){monthDisp.innerHTML = 'March';}
+  else if(mon == 3){monthDisp.innerHTML = 'April';}
+  else if(mon == 4){monthDisp.innerHTML = 'May';}
+  else if(mon == 5){monthDisp.innerHTML = 'June';}
+  else if(mon == 6){monthDisp.innerHTML = 'July';}
+  else if(mon == 7){monthDisp.innerHTML = 'August';}
+  else if(mon == 8){monthDisp.innerHTML = 'September';}
+  else if(mon == 9){monthDisp.innerHTML = 'October';}
+  else if(mon == 10){monthDisp.innerHTML = 'November';}
+  else if(mon == 11){monthDisp.innerHTML = 'December';}
+  monthDisp.innerHTML += ' ' + (year + 1900);
+  }
 
 
 function assigners(){
@@ -30,5 +47,5 @@ function assigners(){
 (function init(){
   createGeneral();
   assigners();
-  getMonth();
+  getMonth(d.getMonth(), d.getYear());
 })()
